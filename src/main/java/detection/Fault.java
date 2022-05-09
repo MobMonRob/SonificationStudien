4e45d5b6ff77e6f0ddf918a798b95c92a177b4be
n.hartmann <noahhartmann13@icloud.com>
2022-05-09 12:32:58 +0200

    Changed coordinates list parameter in Fault to marker list

--- src/main/java/detection/Fault.java
+++ src/main/java/detection/Fault.java
@@ -2,9 +2,9 @@ package detection;
 
 import java.util.List;
 
-import marker.Coordinates;
+import marker.Marker;
 
-public record Fault(FaultSeverity fault, String description, List<Coordinates> coordinates)
+public record Fault(FaultSeverity fault, String description, List<Marker> markers)
 {
-
+	
 }
